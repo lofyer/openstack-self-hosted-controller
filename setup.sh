7613e789d90d7f2e163e7cf4d57f06582aaeed1f
lofyer <lofyer@gmail.com>
2017-05-11 22:45:24 +0800

    update

--- setup.sh
+++ setup.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
+echo "Follow instructions below.\n"
 
+echo "1. Create a shared storage\n"
+echo "2. P2V your controller\n"
+echo "3. Modify start-vm.sh and run, then change network configuration of your vm controller\n"
+echo "4. Copy arbitration-host.py to your host, vm-agent.py to your vm\n"
+echo "5. \n"
