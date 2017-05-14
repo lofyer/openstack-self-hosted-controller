468b137a0efe88052c66ace62ec34a7ae19a6eca
lofyer <lofyer@gmail.com>
2017-05-14 10:49:14 +0800

    update

--- vm-agent.py
+++ vm-agent.py
@@ -1,2 +1,15 @@
 #!/usr/bin/env python2
+import sys
+import etcd
 
+try:
+    client = etcd.Client(host='127.0.0.1', port=4001)
+except Exception as e:
+    print("Etcd client init error: {}").format(e)
+
+try:
+    lock = client.get_lock("/test/lock")
+except Exception as e:
+    print("Get lock error: {}").format(e)
+
+sys.exit(0)
